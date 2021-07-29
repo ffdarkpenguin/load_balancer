426a52451f88bcbebb65105ef45bb4ef0c414887
= <=>
2021-07-29 14:45:58 -0300

    review: clear trash in app

--- src/app.py
+++ src/app.py
@@ -32,7 +32,6 @@ def validate_parameters():
         usage()
     if param_count == 3:
         if "DOCKER" in environ and environ["DOCKER"] == "True":
-            print("no docker")
             print("While running this app in a Docker container it's not allowed to use output "
                   "parameter.")
             usage()
